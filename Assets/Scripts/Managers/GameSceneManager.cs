5dd091fb73c6863a4ad1f89237a4bfe80df836c0
Linh Huỳnh Lê Đan <22520759@ms.uit.edu.vn>
2024-07-18 22:55:37 +0700

    Switching Scenes

---
+++ Assets/Scripts/Managers/GameSceneManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene(string SceneName)
+    {
+        Debug.Log("LOADING "+SceneName);
+        SceneManager.LoadScene(SceneName);
+    }    
+}
