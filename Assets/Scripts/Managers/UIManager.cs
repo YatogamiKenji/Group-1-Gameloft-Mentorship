dfccd984b9fb4ba38fd11970b58b087e58ac0732
ntmynhu <157496817+ntmynhu@users.noreply.github.com>
2024-07-18 17:18:11 +0700

    Update handling TimerPanel and TimerText

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -8,22 +8,39 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField] private VoidPublisherSO resumeGameSO;
     [SerializeField] private VoidPublisherSO pauseGameSO;
+
     [SerializeField] private TextMeshProUGUI timerText;
     [SerializeField] private TextMeshProUGUI resultMessageText;
+
     [SerializeField] private GameObject pausePanel;
     [SerializeField] private GameObject resultPanel;
+    [SerializeField] private Image timeSlider;
+
+    //Totaltime của game được set để chạy trong update 
+    private float totalTime;
+    private float time;
 
     private void Start()
     {
-        //Deactivate pausePanel và resultPanel
         pausePanel.SetActive(false);
         resultPanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        UpdateTimerDisplay();
     }
 
     private void OnEnable()
     {
         resumeGameSO.OnEventRaised += ClosePausePanel;
         pauseGameSO.OnEventRaised += ShowPausePanel;
+    }
+
+    private void OnDisable()
+    {
+        resumeGameSO.OnEventRaised -= ClosePausePanel;
+        pauseGameSO.OnEventRaised -= ShowPausePanel;
     }
 
     private void ShowPausePanel()
@@ -46,22 +63,37 @@ public class UIManager : MonoBehaviour
 
     }
 
-    private void UpdateTimerDisplay(float time)
-    {
-
-    }
-
-    private void ShowResultMessage(string message)
-    {
-
-    }
-
     private void CloseOptionMenu()
     {
 
     }
 
     private void ShowOptionMenu()
+    {
+
+    }
+
+    //Set total time từ khi bắt đầu game để chạy timer
+    private void SetTotalTime(float totalTime)
+    {
+        this.totalTime = totalTime;
+        time = totalTime;
+    }
+
+    private void UpdateTimerDisplay()
+    {
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+            int minutes = (int)(time / 60);
+            int seconds = (int)(time % 60);
+            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+            timeSlider.fillAmount = time / totalTime;
+        }
+    }
+
+    private void ShowResultMessage(string message)
     {
 
     }
