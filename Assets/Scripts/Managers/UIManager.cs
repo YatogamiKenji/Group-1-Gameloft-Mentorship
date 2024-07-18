24478f966f8a426531a2496c901383650464e15b
ntmynhu <157496817+ntmynhu@users.noreply.github.com>
2024-07-18 14:16:52 +0700

    Update UIManager.cs

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,8 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class UIManager : MonoBehaviour
+public class UIManager : VoidEventListener
 {
-    
+    [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private TextMeshProUGUI resultMessageText;
+
+    private void UpdateTimerDisplay(float time)
+    {
+
+    }
+
+    private void ShowResultMessage(string message)
+    {
+
+    }
+
+    private void CloseOptionMenu()
+    {
+
+    }
+
+    private void ShowOptionMenu()
+    {
+
+    }
 }
