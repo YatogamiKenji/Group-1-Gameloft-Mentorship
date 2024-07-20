a0e3a7363555dc820a6276bfb666a0e03eec019e
Linh Huỳnh Lê Đan <22520759@ms.uit.edu.vn>
2024-07-20 11:46:10 +0700

    Audio Volume Setting by slider

--- Assets/Scripts/Units/SoundSetting.cs
+++ Assets/Scripts/Units/SoundSetting.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundSetting : MonoBehaviour
 {
@@ -18,14 +19,20 @@ public class SoundSetting : MonoBehaviour
 
     public void OnMusicUpdate(float value) {
         Debug.Log("Music: " + value);
+        AudioManager.Instance.UpdateMixerVolume(value, -1);
     }
     public void OnSfxUpdate(float value) { 
         Debug.Log("SFX: " + value);
+        AudioManager.Instance.UpdateMixerVolume(-1, value);
     }
 
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
     public void OpenPanel()
     {
         this.gameObject.SetActive(true);
+        musicSlider.value = AudioManager.Instance.GetValue("Music Volume") / 100f;
+        sfxSlider.value = AudioManager.Instance.GetValue("SFX Volume") / 100f;
     }    
     public void ClosePanel()
     {
